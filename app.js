2f6885074c08b25fe0f96d9363f6c838659cab0c
Pany Koutsios <pkoutsios@hotmail.com>
2014-05-28 13:29:49 +0100

    Final
    Ready to be uploaded

--- app.js
+++ app.js
@@ -1,28 +1,64 @@
-$(document).ready(function () {
+$(document).ready(function() {
 	// Add item
-	$("#addbutton").mousedown(addItem);
+	$("#addbutton").mousedown(function() {
+		addItem();
+		$("#addbox").focus();
+	});
 	
 	$(document).keydown(function(event) {
 		if (event.which == 13) {
 			addItem();
 		}
-	})
+	});
 	
 	// Remove item
-	$("#thelist").on("click", ".del", function (){
+	$("#thelist").on("click", ".del", function(){
 		$(this).parent().remove();
+		checkForLastItem();
 	})
 	
 	// Strike item
-	.on("click", ".items", function (event){
+	.on("click", ".items", function(event){
 		$(this).toggleClass("checked");
+	});
+	
+	// Show instructions
+	$("#instructions").mouseover(function() {
+		$("#instructions-info").show();
+	})
+	$("#instructions").mouseleave(function() {
+		$("#instructions-info").hide();
+	});
+	
+	// Sortable list (requires extra javascript source in HTML)
+	$("#thelist").sortable();
+	
+	// New list
+	$("#newbutton").mousedown(function() {
+		var retVal = confirm("Creating a new list will delete your current list.\n\nDo you want to continue?");
+		if( retVal == true ){
+			$("#thelist").children().remove();
+			checkForLastItem();
+			return true;
+		}
 	});
 });
 
+// add item to list
 function addItem () {
-	var item = '<li><div class="del"></div><div class="items">' + $("#addbox").val() + '</div></li>';
-	if (item !="") {
+	var addbox = $("#addbox").val().trim();
+	if (addbox != "") {
+		$("#example-item").remove();
+		var item = '<li><div class="del"></div><div class="items">' + addbox + '</div></li>';
 		$("#thelist").prepend (item);
 		$("#addbox").val("");
 	}
 };
+
+// check for last item and add the example text
+function checkForLastItem () {
+	var countli = $("#thelist li").length;
+	if (countli == 0) {
+		$("#thelist").prepend ('<li id="example-item" class="example">(Waiting for your items)</li>');
+	}
+}
